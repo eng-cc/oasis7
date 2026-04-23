539cdf2026fbbeb31245f0b880940c3dafd0515d
chengcheng <66scc66@gmail.com>
2026-04-23 09:48:19 +0800

    test: address launcher required gate comments

--- scripts/ci-tests.sh
+++ scripts/ci-tests.sh
@@ -185,7 +185,7 @@ case "$tier" in
     run_required_component "oasis7_viewer tests" "${OASIS7_CI_RUN_VIEWER_TESTS:-}" run_oasis7_viewer_tests
     run_required_component "viewer software-safe contract" "${OASIS7_CI_RUN_VIEWER_CONTRACT_TESTS:-}" run_oasis7_viewer_software_safe_feedback_contract_tests
     run_required_component "viewer wasm check" "${OASIS7_CI_RUN_VIEWER_WASM_CHECK:-}" run_oasis7_viewer_wasm_check
-    run_required_component "launcher web build" "${OASIS7_CI_RUN_LAUNCHER_WEB_BUILD:-}" run_oasis7_client_launcher_web_build
+    run_required_component "launcher web build" "${OASIS7_CI_RUN_LAUNCHER_WEB_BUILD:-false}" run_oasis7_client_launcher_web_build
     ;;
   full)
     run_full_core_tier_tests
